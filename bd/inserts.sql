dd3165dc80a4ae2fb504fb020dd8bdd2b87e00b3
Carlos Hemrique <c1291391@bb.com.br>
2018-06-05 11:27:36 -0300

    igiadf

---
+++ bd/inserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO `plat_ofic`.`perfil` (`perfil`) VALUES ('Máster');
+INSERT INTO `plat_ofic`.`perfil` (`perfil`) VALUES ('Sênior');
+INSERT INTO `plat_ofic`.`perfil` (`perfil`) VALUES ('Pleno');
+INSERT INTO `plat_ofic`.`perfil` (`perfil`) VALUES ('Júnio');
+INSERT INTO `plat_ofic`.`perfil` (`perfil`) VALUES ('Trainee');
